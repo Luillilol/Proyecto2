041495aef0f7c90195e52707f3037566a1506cac
Luna <lunifer2004@gmail.com>
2021-06-01 20:23:55 -0500

    FEAT: funcionan direcciones volver

--- dynamics/js/creditos.js
+++ dynamics/js/creditos.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", ()=>{
   let volver = document.getElementById("volver");
   volver.addEventListener("click", ()=>{
-     window.location.assign("../../index.html");
+     window.location.assign("../index.html");
   })
   volver.addEventListener("keydown", (e)=>{
     console.log(e.key);
     if(e.key == "Enter"){
-      window.location.assign("../../index.html");
+      window.location.assign("../index.html");
     }
   });
 
--- dynamics/js/puntos.js
+++ dynamics/js/puntos.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", ()=>{
     let vuelve = document.getElementById('volver');
-    let un = document.getElementById('uno';)
+    let un = document.getElementById('uno');
     vuelve.addEventListener("click", ()=>{
-    window.location.assign("../../index.html");
+    window.location.assign("../index.html");
   
     
   })
   vuelve.addEventListener("keydown", (e)=>{
     if(e.key == "Enter"){
-      window.location.assign("../../index.html");
+      window.location.assign("../index.html");
     }
   })
   un.setAttribute()
